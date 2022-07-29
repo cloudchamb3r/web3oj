3ca6f9a6d1f00d1e11c1b45d18f910f215b2ac28
jjajjajja <jizon0123@protonmail.com>
2022-07-29 10:53:43 +0900

    sol array sum opt

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,29 +2,31 @@ import { HardhatUserConfig } from "hardhat/config";
 import "hardhat-gas-reporter";
 import { config as dotenv } from "dotenv";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
 
 dotenv();
 
-const config: HardhatUserConfig = {
+const config = {
   solidity: {
-    compilers: [{ version: "0.8.9" }],
-    settings:{
+    compilers: [{ version: "0.8.15" }],
+    settings: {
       optimizer: {
         enabled: true,
-        runs: 3000,
-      }
-    }
+      },
+    },
   },
   networks: {
     rinkeby: {
       url: process.env.RINKEBY_URL,
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts: [process.env.PRIVATE_KEY],
+      gas: 2100000,
+      blockGasLimit: 30_000_000,
+      minGasPrice: "0",
     },
   },
-  gasReporter: {
-    currency: "USD",
-    enabled: true,
+  etherscan: {
+    // your api key for etherscan
+    apiKey: process.env.ETHERSCAN_KEY,
   },
 };
 
--- scripts/sol_array_sum.ts
+++ scripts/sol_array_sum.ts
@@ -1,28 +1,14 @@
-import {ethers} from 'hardhat';
+import { ethers } from "hardhat";
 
-(async function() {
-    const [player] = await ethers.getSigners();
-    const addy = '0xD4AD935e6D626CafD45836d04821Cc709311289e';
-    const contract = await ethers.getContractAt('SumOfArrayProblem', addy, player);
-    const SumArr = await ethers.getContractFactory('MySumOfArray');
-    const sumArr = await SumArr.connect(player).deploy();
-    await sumArr.deployed();
-    await contract.setSumOfArrayContract(sumArr.address);
-    
-    console.log(sumArr.address);
-    const gas_price = await player.getGasPrice();
-    const arr = [
-        1,2,3,4,5,6,7,8,9,10,
-        1,2,3,4,5,6,7,8,9,10,
-        1,2,3,4,5,6,7,8,9,10,
-        1,2,3,4,5,6,7,8,9,10,
-        1,2,3,4,5,6,7,8,9,10,
-        1,2,3,4,5,6,7,8,9,10,
-        1,2,3,4,5,6,7,8,9,10,
-        1,2,3,4,5,6,7,8,9,10,
-        1,2,3,4,5,6,7,8,9,10,
-        1,2,3,4,5,6,7,8,9,10,
-    ]
-    console.log(gas_price);
-    console.log(await sumArr.estimateGas.sum(arr));
-})().catch(err => console.error(err));
+(async function () {
+  const [player] = await ethers.getSigners();
+  const addy = "0x9A6dF8Fbf58aecA875BBa3cf02b255277C6f1580";
+  const contract = await ethers.getContractAt(
+    "SumOfArrayProblem",
+    addy,
+    player
+  );
+  const SumArr = await ethers.getContractFactory("MySumOfArray");
+  const sumArr = await SumArr.connect(player).deploy();
+  await contract.setSumOfArrayContract(sumArr.address);
+})().catch((err) => console.error(err));
